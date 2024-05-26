771b6d7356dbdd781aa5aa1a4b0b3434ed73626a
elnataCorrea <elnata_correa@hotmail.com>
2024-05-26 19:59:21 -0400

    foi criado as paginas
    foi criado as paginas 'book_box' em html e css, assim como tambem foi criada as paginas 'holodecks'tambem em html e css, alem disso fiz uma alteraçao no js, possibilitando o acesso da pagina de projetos e criei a rolagem scroll no header de forma mais suave

--- cd.js/menu.js
+++ cd.js/menu.js
@@ -1,5 +1,19 @@
+window.addEventListener("scroll", function() {
+    let header = document.querySelector('.header');
+    header.classList.toggle('rolagem', window.scrollY > 20);
+});
 
-window.addEventListener("scroll", function(){
-    let header = this.document.querySelector('.header')
-    header.classList.toggle('rolagem',window.scrollY > 20)
-})
+
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const href = this.getAttribute('href');
+        if (href.startsWith('#')) {
+            document.querySelector(href).scrollIntoView({
+                behavior: 'smooth'
+            });
+        } else {
+            window.location.href = href;
+        }
+    });
+});
